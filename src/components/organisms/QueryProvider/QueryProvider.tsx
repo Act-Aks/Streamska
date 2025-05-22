930704c4066798db1a6e5907d5e9f8c9ebeca4c5
Act-Aks <akashsrivastava.git@gmail.com>
2025-05-22 12:52:52 +0530

    fix: remove dev tools for react query

--- src/components/organisms/QueryProvider/QueryProvider.tsx
+++ src/components/organisms/QueryProvider/QueryProvider.tsx
@@ -1,16 +1,10 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { PropsWithChildren } from 'react'
 
 const queryClient = new QueryClient()
 
 const QueryProvider: React.FC<PropsWithChildren> = ({ children }) => {
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-            <ReactQueryDevtools initialIsOpen={false} />
-        </QueryClientProvider>
-    )
+    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 }
 
 export default QueryProvider
